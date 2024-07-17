db3c5547629e3b603411a5165942cefa76305dc9
Atharva <atharva.malik@gmail.com>
2024-07-17 16:11:35 +1000

    added most of the chopping code

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -17,6 +17,9 @@ public class SelectionManager : MonoBehaviour
 
     public bool handIsVisible;
 
+    public GameObject selectedTree;
+    public GameObject chopHolder;
+
     private void Awake() {
         if(Instance != null && Instance != this){
             Destroy(gameObject);
@@ -42,6 +45,20 @@ public class SelectionManager : MonoBehaviour
             var selectionTransform = hit.transform;
             
             InteractableObject interactable = selectionTransform.GetComponent<InteractableObject>();
+
+            ChoppableTree choppableTree = selectionTransform.GetComponent<ChoppableTree>();
+
+            if(choppableTree && choppableTree.playerInRange){
+                choppableTree.canBeChopped = true;
+                selectedTree = choppableTree.gameObject;
+                chopHolder.gameObject.SetActive(true);
+            }else{
+                if (selectedTree != null){
+                    selectedTree.gameObject.GetComponent<ChoppableTree>().canBeChopped = false;
+                    selectedTree = null;
+                    chopHolder.gameObject.SetActive(false);
+                }
+            }
 
             if (interactable && hit.distance <= 7 && interactable.playerInRange)
             {
