2def51a82886196ed9bb70d25292d7150d263fce
Atharva <atharva.malik@gmail.com>
2024-07-17 20:19:39 +1000

    added everything but bugs

--- Assets/Scripts/ChoppableTree.cs
+++ Assets/Scripts/ChoppableTree.cs
@@ -11,10 +11,13 @@ public class ChoppableTree : MonoBehaviour
 
     public float treeMaxHealth;
     public float treeHealth;
+    public Animator animator;
 
     private void Start() {
         treeHealth = treeMaxHealth;
+        animator = transform.parent.transform.parent.GetComponent<Animator>();
     }
+
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")){
@@ -35,7 +38,23 @@ public class ChoppableTree : MonoBehaviour
     private IEnumerator hit()
     {
         yield return new WaitForSeconds(0.5f);
+        animator.SetTrigger("shake");
         treeHealth -= 1;
+        if (treeHealth <= 0){
+            treeIsDead();
+        }
+    }
+
+    private void treeIsDead()
+    {
+        Vector3 treePosition = transform.position;
+        Destroy(transform.parent.transform.parent.gameObject);
+        canBeChopped = false;
+        SelectionManager.Instance.selectedTree = null;
+        SelectionManager.Instance.chopHolder.gameObject.SetActive(false);
+
+        GameObject brokenTree = Instantiate(Resources.Load<GameObject>("ChoppedTree"), new Vector3(treePosition.x, treePosition.y+0.1f, treePosition.z), Quaternion.Euler(0,0,0));
+
     }
 
     private void Update() {
